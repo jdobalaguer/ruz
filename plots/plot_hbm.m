72026af2ed311191cb5de8641cd78e2f34fc8545
jan <jan@deepmind.com>
2013-11-26 14:11:24 +0000

    supp materials

--- plots/plot_hbm.m
+++ plots/plot_hbm.m
@@ -3,16 +3,19 @@
 load_twoparams;
 run_hbm_model;
 
-%% performance plot
+%% plot
 
 %figure
 figure('color',[1 1 1]);
 c = 'rg';
 
+% performance
+i_subplot = 0;
 for i_novelty = 1:numbers.nb_novelties
 
-    % figure
-    subplot(1,numbers.nb_novelties,i_novelty);
+    % plot
+    i_subplot = i_subplot+1;
+    subplot(2,numbers.nb_novelties,i_subplot);
     hold on;
     
     this_subdata = nan(1,16);
@@ -29,24 +32,21 @@ for i_novelty = 1:numbers.nb_novelties
     end
     
     % plot
-    plot(this_subdata, 'o','markerfacecolor','k','markeredgecolor','k','markersize',10);
+    plot(this_subdata, 'o','markerfacecolor',c(i_novelty),'markeredgecolor',c(i_novelty),'markersize',10,'color',c(i_novelty));
     plot(this_moddata,'linewidth',2,'linestyle','--','color',c(i_novelty));
     plot(this_optdata,'linewidth',2,'linestyle','-','color',c(i_novelty));
-    ylim([0 1]);
+    ylim([0.4 1]);
     xlim([1 numbers.nb_trials]);
+    set(gca,'ytick',0.4:0.2:1);
     set(gca,'fontsize',16,'fontname','Arial');
 end
 
-%% response plot
-
-%figure
-figure('color',[1 1 1]);
-c = 'rg';
-
+% response
 for i_novelty = 1:numbers.nb_novelties
 
-    % figure
-    subplot(1,numbers.nb_novelties,i_novelty);
+    % plot
+    i_subplot = i_subplot+1;
+    subplot(2,numbers.nb_novelties,i_subplot);
     hold on;
     
     this_subdata = nan(1,16);
@@ -63,11 +63,12 @@ for i_novelty = 1:numbers.nb_novelties
     end
     
     % plot
-    plot(this_subdata, 'o','markerfacecolor','k','markeredgecolor','k','markersize',10);
+    plot(this_subdata, 'o','markerfacecolor',c(i_novelty),'markeredgecolor',c(i_novelty),'markersize',10);
     plot(this_moddata,'linewidth',2,'linestyle','--','color',c(i_novelty));
     plot(this_optdata,'linewidth',2,'linestyle','-','color',c(i_novelty));
-    ylim([0 1]);
+    ylim([0.4 1]);
     xlim([1 numbers.nb_trials]);
+    set(gca,'ytick',0.4:0.2:1);
     set(gca,'fontsize',16,'fontname','Arial');
 end
 
@@ -93,9 +94,9 @@ for i_novelty = 1:numbers.nb_novelties
     end
     
     % plot
-    plot(ones(1,16)  ,'linewidth',1,'linestyle','-','color','k');
-    plot(this_moddata,'linewidth',2,'linestyle','-','color',c(i_novelty));
-    ylim([0 36]);
+    %plot(ones(1,16)  ,'linewidth',1,'linestyle','-','color','k');
+    plot(this_moddata,'linewidth',2,'linestyle','--','color',c(i_novelty));
+    ylim([0 40]);
     xlim([1 numbers.nb_trials]);
     set(gca,'fontsize',16,'fontname','Arial');
 end
@@ -124,10 +125,12 @@ for i_novelty = 1:numbers.nb_novelties
     end
     
     % plot
+    plot(0.5*ones(1,16),'linewidth',1,'linestyle',':','color',[.7,.7,.7]);
     plot(this_truedata,'linewidth',1,'linestyle','-','color','k');
-    plot(this_moddata,'linewidth',2,'linestyle','-','color',c(i_novelty));
-    ylim([0 1]);
+    plot(this_moddata,'linewidth',2,'linestyle','--','color',c(i_novelty));
+    ylim([0.4 0.6]);
     xlim([1 numbers.nb_trials]);
+    set(gca,'ytick',0.4:0.1:0.6);
     set(gca,'fontsize',16,'fontname','Arial');
 end
 
