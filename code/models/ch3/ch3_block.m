6f8f29181b30c92ab29eef87cdaa19d6240ebcfa
jan <jan@deepmind.com>
2014-06-04 00:10:34 +0100

    fix model ch3

--- code/models/ch3/ch3_block.m
+++ code/models/ch3/ch3_block.m
@@ -77,8 +77,8 @@ for i_trial = 1:nb_trial
     %% learning
     
     % step
-    if (~target); alpha = model.alpha_m * (1 - model.alpha_r); end
-    if ( target); alpha = model.alpha_m * (    model.alpha_r); end
+    if (~choice); alpha = model.alpha_m * (1 - model.alpha_r); end
+    if ( choice); alpha = model.alpha_m * (    model.alpha_r); end
     
     % boundary
     boundary = -1 + 2*target; % +1 if correct, -1 if wrong
